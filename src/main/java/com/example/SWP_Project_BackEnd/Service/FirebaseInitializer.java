7b1ed13ecebdd0a8ab8758159cb4dfa1f8c56c86
Quan Tong <quankun2303@gmail.com>
2024-11-08 21:20:30 +0700

    little fix

--- src/main/java/com/example/SWP_Project_BackEnd/Service/FirebaseInitializer.java
+++ src/main/java/com/example/SWP_Project_BackEnd/Service/FirebaseInitializer.java
@@ -3,6 +3,7 @@ package com.example.SWP_Project_BackEnd.Service;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -14,8 +15,7 @@ public class FirebaseInitializer {
 
     @PostConstruct
     public void initialize() throws IOException {
-        FileInputStream serviceAccount =
-                new FileInputStream("C:\\Users\\ASUS GAMING\\Documents\\FPT University\\FALL 2024\\SWP391\\SWP_Project_BackEnd\\SWP_Project_BackEnd\\koi-trip-system-firebase-adminsdk-i7lk4-8afa605208.json");
+        FileInputStream serviceAccount = new FileInputStream(new ClassPathResource("koi-trip-system-firebase-adminsdk-i7lk4-8afa605208.json").getFile());
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
